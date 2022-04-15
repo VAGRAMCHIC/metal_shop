754c857b2d5602a272b1e3ae26290af958baf0dc
VAGRAMCHIC <vagramchic@gmail.com>
2022-04-15 12:08:20 +0300

    yolo

--- metal_shop/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.preview, name='prewiew'), 
-    path('catalog', views.catalog, name='catalog'),
-    path('catalog/<int:product_id>', views.show_product, name='show_product'),
-
-]
